5df3c448036c38207d462542a24c4f350acc964e
TomerY <tommery@gmail.com>
2025-11-23 15:16:53 +0200

    fix design

---
+++ src/main/resources/static/js/register.js
@@ -0,0 +1,7 @@
+function openRegisterModal() {
+    document.getElementById("registerModal").style.display = "block";
+}
+
+function closeRegisterModal() {
+    document.getElementById("registerModal").style.display = "none";
+}
